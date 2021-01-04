374ef5f51bfa28ec4d78b16cd987c6c5420101b4
Theo Sun <theo.sun@sap.com>
2021-01-04 15:45:58 +0800

    feat: add proxy

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import fetch from "node-fetch";
 import path from "path";
 
 const app = express();
@@ -7,7 +8,13 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../static")));
 
-app.get("/name", (req) => req.res.json({ name: process.env.SERVICE_NAME || "default" }));
+app.get("/status", (req) => req.res.json({ name: process.env.SERVICE_NAME || "default" }));
+
+app.get(/^\/proxy\/(.*?)$/, (req) => {
+  fetch(req.params[0])
+    .then(response => response.body.pipe(req.res))
+    .catch(req.next);
+});
 
 app.listen(parseInt(process.env.PORT || '3000', 10));
 
