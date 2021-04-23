c5a39af9f7ff9ce17538d9df77cf69b868c2a1f9
Theo Sun <theo.sun@sap.com>
2021-04-23 16:07:19 +0800

    chore(dep): update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
+import { debug } from "debug";
 import express from "express";
 import fetch from "node-fetch";
 import path from "path";
+import { inspect } from "util";
 
+const logger = debug("web-server");
 const app = express();
 
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../static")));
 
-app.get("/status", (req) => req.res.json({ name: process.env.SERVICE_NAME || "default" }));
+const serviceName = process.env.SERVICE_NAME ?? "default";
+
+logger(`service name: ${serviceName}`);
+
+app.get("/status", (req) => req.res.json({
+  name: serviceName
+}));
 
 app.get(/^\/proxy\/(.*?)$/, (req) => {
   fetch(req.params[0])
@@ -16,5 +25,10 @@ app.get(/^\/proxy\/(.*?)$/, (req) => {
     .catch(req.next);
 });
 
-app.listen(parseInt(process.env.PORT || '3000', 10));
+const listener = app.listen(
+  parseInt(process.env.PORT || '3000', 10),
+  () => {
+    logger(`serve service '${serviceName}' on ${inspect(listener.address())}`);
+  }
+);
 
